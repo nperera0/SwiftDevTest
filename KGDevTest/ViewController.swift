21512603786284b899d6f81e7a8e7778c96a4f94
Nisal Perera <nisalperera0@gmail.com>
2015-12-22 11:02:11 -0500

    Initial Commit

---
+++ KGDevTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KGDevTest
+//
+//  Created by Nisal Perera on 2015-12-22.
+//  Copyright © 2015 Nisal Perera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
